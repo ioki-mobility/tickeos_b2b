569577e65e5ace4eb4dade87d806d4f46b2f22e2
Tobias Matz <tobias.matz@ioki.com>
2020-09-02 15:08:32 +0200

    Add tickeos_b2b spec

--- spec/tickeos_b2b_spec.rb
+++ spec/tickeos_b2b_spec.rb
@@ -1,9 +1,12 @@
-RSpec.describe Tickeos::B2b do
-  it "has a version number" do
-    expect(Tickeos::B2b::VERSION).not_to be nil
-  end
+# frozen_string_literal: true
 
-  it "does something useful" do
-    expect(false).to eq(true)
+require 'spec_helper'
+
+RSpec.describe TickeosB2b do
+  let(:version) { '0.1.0' }
+
+  it 'has the correct version number' do
+    expect(TickeosB2b::VERSION).not_to be nil
+    expect(TickeosB2b::VERSION).to eq(version)
   end
 end
