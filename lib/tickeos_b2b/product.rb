f51d1fa20df4961ab1cf770d78ec5a0c58680b14
Tobias Matz <tobias.matz@ioki.com>
2020-09-28 20:00:49 +0200

    Update Product class - Require securerandom gem - Add personalize method - Add load_product_data method

--- lib/tickeos_b2b/product.rb
+++ lib/tickeos_b2b/product.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'securerandom'
+
 module TickeosB2b
   class Product
     ATTRIBUTES = [
@@ -7,7 +9,16 @@ module TickeosB2b
       :reference_id,
       :updated_at,
       :published,
-      :id
+      :id,
+      :vu_name,
+      :vu_role,
+      :sort_order,
+      :tariff_zone_count,
+      :tariff_zone_count_required,
+      :sale_date_from,
+      :sale_date_to,
+      :distribution_method,
+      :visible
     ].freeze
 
     attr_accessor(*ATTRIBUTES)
@@ -22,6 +33,16 @@ module TickeosB2b
       ATTRIBUTES.map do |attr|
         [attr, public_send(attr)]
       end.to_h
+    end
+
+    def personalize(personalization_data = {})
+      ticket                      = Ticket.new(personalization_data)
+      ticket.product              = self
+      ticket.product_reference_id = reference_id
+      ticket.serial_ordering      = "ord_#{SecureRandom.uuid}"
+      ticket.transaction_id       = "tra_#{SecureRandom.uuid}"
+
+      ticket
     end
 
     class << self
@@ -44,6 +65,33 @@ module TickeosB2b
 
       def published?(product)
         product == '1'
+      end
+
+      def load_product_data(product, json)
+        json = json['TICKeosProxy']['txProductDataResponse']['Product']
+
+        product.id                         = json['@id']
+        product.vu_name                    = json['@vu_name']
+        product.vu_role                    = json['@vu_role']
+        product.sort_order                 = json['@sort_order']
+        product.tariff_zone_count          = json['@tariff_zone_count']
+        product.tariff_zone_count_required = json['@tariff_zone_count_required']
+        product.sale_date_from             = to_date(json['@sale_date_from'])
+        product.sale_date_to               = to_date(json['@sale_date_to'])
+        product.distribution_method        = json['@distribution_method']
+        product.visible                    = visible?(json['@visible'])
+
+        product
+      end
+
+      def visible?(input)
+        input == '1'
+      end
+
+      def to_date(input)
+        return '' if input.blank?
+
+        Time.parse(DateTime.parse(input).to_s)
       end
     end
   end
