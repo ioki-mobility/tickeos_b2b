d9a5c557b2457dec9844074e43d10556b91b2d23
Tobias Matz <tobias.matz@ioki.com>
2020-10-29 10:55:39 +0100

    Fix failing spec

--- spec/tickeos_b2b/api/cancel_spec.rb
+++ spec/tickeos_b2b/api/cancel_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe TickeosB2b::Api::Cancel do
     <<~XML
       <?xml version=\"1.0\"?>
       <TICKeosProxy apiVersion=\"\" version=\"\" instanceName=\"\">
-        <txCancelRequest go=\"1\" context=\"merchant\" orderingSerial=\"42\"/>
+        <txCancelRequest go=\"1\" context=\"merchant\" orderProductSerial=\"42\"/>
       </TICKeosProxy>
     XML
   end
 
-  let(:operation) { cancel.request_body(server_ordering_serial) }
-  let(:server_ordering_serial) { '42' }
+  let(:operation) { cancel.request_body(server_order_product_serial) }
+  let(:server_order_product_serial) { '42' }
 
   describe '#request_body' do
     it 'returns the correct xml request body' do
