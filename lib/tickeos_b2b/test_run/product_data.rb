263371b6a6e07d29dd8979fab8d1decee8064e5d
Tobias Matz <tobias.matz@ioki.com>
2020-12-04 13:45:08 +0100

    Add helper classes for each Client method

---
+++ lib/tickeos_b2b/test_run/product_data.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module TickeosB2b
+  module TestRun
+    class ProductData
+      def self.load!(options, reference_id)
+        options = options.dig(:product_data, reference_id.to_sym)
+
+        return Error if options.nil?
+
+        Nori.new.parse(
+          Nokogiri::XML::Builder.new do |xml|
+            xml.TICKeosProxy(apiVersion: '1.0', version: Time.now.strftime('%Y-%m-%d'), instanceName: 'test') do
+              xml.txProductDataResponse(productReferenceId: '', timeIntervalOptionType: 'type') do
+                xml.Product(
+                  id:                         options[:id],
+                  vu_name:                    options[:vu_name],
+                  vu_role:                    options[:vu_role],
+                  sort_order:                 options[:sort_order],
+                  tariff_zone_count:          options[:tariff_zone_count],
+                  tariff_zone_count_required: options[:tariff_zone_count_required],
+                  sale_date_from:             options[:sale_date_from],
+                  sale_date_to:               options[:sale_date_to],
+                  distribution_method:        options[:distribution_method],
+                  visible:                    options[:visible]
+                )
+              end
+            end
+          end.to_xml
+        )
+      end
+    end
+  end
+end
---
+++ lib/tickeos_b2b/test_run/product_list.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module TickeosB2b
+  module TestRun
+    class ProductList
+      def self.product_list(options = {})
+        options = options.transform_keys(&:to_sym).dig(:product_list)
+
+        Nori.new.parse(
+          Nokogiri::XML::Builder.new do |xml|
+            xml.TICKeosProxy(apiVersion: '1.0', version: Time.now.strftime('%Y-%m-%d'), instanceName: 'test') do
+              xml.txProductResponse(method: 'product_list') do
+                options.each do |opt|
+                  opt = opt.transform_keys(&:to_sym)
+                  xml.productItem(
+                    name:         opt[:name],
+                    reference_id: opt[:reference_id],
+                    updated_at:   '1354181759',
+                    published:    '1'
+                  )
+                end
+              end
+            end
+          end.to_xml
+        )
+      end
+    end
+  end
+end
